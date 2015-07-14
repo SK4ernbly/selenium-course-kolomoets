596a092d519a98794cf24114a01b0a02c873ced3
Sergey Kolomoets <sk4ernbly@gmail.com>
2015-07-14 14:55:20 +0300

    improve_14072015

--- src/test/java/selenium_course_kolomoets/LoginTest.java
+++ src/test/java/selenium_course_kolomoets/LoginTest.java
@@ -15,7 +15,7 @@ public class LoginTest extends TestBase {
   private StringBuffer verificationErrors = new StringBuffer();
 
   @Test
-  public void testUntitled() throws Exception {
+  public void testLogin() throws Exception {
     driver.get(baseUrl + "/php4dvd/");
     driver.findElement(By.id("username")).clear();
     driver.findElement(By.id("username")).sendKeys("admin");
